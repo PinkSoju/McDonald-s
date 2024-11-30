390e36704b5119f8658bf6b1aba79988cb786f30
PinkSoju <yjs4083@naver.com>
2024-11-30 21:06:58 +0900

    Initial Commit

---
+++ McDonald's/McDonald_sApp.swift
@@ -0,0 +1,17 @@
+//
+//  McDonald_sApp.swift
+//  McDonald's
+//
+//  Created by 윤준성 on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct McDonald_sApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ McDonald'sTests/McDonald_sTests.swift
@@ -0,0 +1,17 @@
+//
+//  McDonald_sTests.swift
+//  McDonald'sTests
+//
+//  Created by 윤준성 on 11/30/24.
+//
+
+import Testing
+@testable import McDonald_s
+
+struct McDonald_sTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
